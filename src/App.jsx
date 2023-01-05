2e72888eb9c35818e0c945b0fa3888562e63ec4f
viettran1010 <viettran10101988@gmail.com>
2023-01-05 07:49:24 +0700

    feat: crowdfunding

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,30 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import "./styles/Home.css";
+import { Route, Routes } from "react-router-dom";
+import { NavBar, SideBar } from "./components";
+import { CampaignDetails, CreateCampaign, Home, Profile } from "./pages";
 
-export default function Home() {
+const App = () => {
   return (
-    <div className="container">
-      <main className="main">
-        <h1 className="title">
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/main.jsx</code>, then modify the{" "}
-          <code className="code">src/App.jsx</code> file!
-        </p>
-
-        <div className="connect">
-          <ConnectWallet />
-        </div>
-
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <SideBar></SideBar>
+      </div>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <NavBar></NavBar>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route
+            path="/create-campaign"
+            element={<CreateCampaign> </CreateCampaign>}
+          />
+          <Route path="/profile" element={<Profile></Profile>} />
+          <Route
+            path="/campaign-details/:id"
+            element={<CampaignDetails></CampaignDetails>}
+          />
+        </Routes>
+      </div>
     </div>
   );
-}
+};
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as NavBar } from "./NavBar";
+export { default as SideBar } from "./SideBar";
+export { default as CustomButton } from "./CustomButton";
+export { default as FormField } from "./FormField";
+export { default as Loader } from "./Loader";
+export { default as DisplayCampaigns } from "./DisplayCampaigns";
+export { default as CountBox } from "./CountBox";
